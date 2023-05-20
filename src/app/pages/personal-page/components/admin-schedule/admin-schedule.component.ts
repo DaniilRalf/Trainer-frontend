1de714b0cad7213ef23dcef2ab59249ba5f28e2b
DaniilRalf <ralf.danya@mail.ru>
2023-05-20 15:30:03 +0700

    fix: schedule;

--- src/app/pages/personal-page/components/admin-schedule/admin-schedule.component.ts
+++ src/app/pages/personal-page/components/admin-schedule/admin-schedule.component.ts
@@ -3,24 +3,40 @@ import {BehaviorSubject, take} from "rxjs"
 import {User} from "../../../../models/types/user"
 import {GraphqlService} from "../../../../helpers/services/graphql.service"
 import {ModalAdminScheduleComponent} from "./modal-admin-schedule/modal-admin-schedule.component"
-import {MatDialog} from "@angular/material/dialog";
+import {MatDialog} from "@angular/material/dialog"
 
 @Component({
   selector: 'app-admin-schedule',
   templateUrl: './admin-schedule.component.html',
   styleUrls: ['./admin-schedule.component.scss']
 })
+
 export class AdminScheduleComponent implements OnInit {
+
+  public monthEnum: {
+    0: 'Январь', 1: 'Ферваль', 2: 'Март',
+    3: 'Апрель', 4: 'Май', 5: 'Июнь',
+    6: 'Июль', 7: 'Август', 8: 'Сентябрь',
+    9: 'Октябрь', 10: 'Ноябрь', 11: 'Декабрь',
+  } = {
+    0: 'Январь', 1: 'Ферваль', 2: 'Март',
+    3: 'Апрель', 4: 'Май', 5: 'Июнь',
+    6: 'Июль', 7: 'Август', 8: 'Сентябрь',
+    9: 'Октябрь', 10: 'Ноябрь', 11: 'Декабрь',
+  }
 
   //TODO: поправить типизацию, отменить все подписки
   calendarData: any[] = []
+
+  nowMonth!: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
 
   @Input() public user!: BehaviorSubject<User>
 
   constructor(
     private graphQlService: GraphqlService,
     private dialog: MatDialog
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.user.pipe(take(1)).subscribe((i: any) => {
@@ -40,6 +56,8 @@ export class AdminScheduleComponent implements OnInit {
   //TODO: types
   private generateCalendarData(users: any): void {
     const nowYear = new Date().getFullYear()
+    //@ts-ignore
+    this.nowMonth = new Date().getMonth()
     const nextMonth = new Date().getMonth() + 1
     const lastDayInMonth = new Date(nowYear, nextMonth, 0).getDate()
     const indexFirstDayInMonth = new Date(nowYear, new Date().getMonth(), 1).getDay()
@@ -71,7 +89,6 @@ export class AdminScheduleComponent implements OnInit {
 
       })
     })
-    // console.log(this.calendarData)
   }
 
   // TODO: types
