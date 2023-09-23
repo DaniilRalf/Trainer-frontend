3f2b450044443b62eb9db77302326e98bd635928
DaniilRalf <ralf.danya@mail.ru>
2023-09-24 00:04:29 +0700

    new_design fix

--- src/app/pages/home-page/components/header-sticky/header-sticky.component.ts
+++ src/app/pages/home-page/components/header-sticky/header-sticky.component.ts
@@ -1,5 +1,4 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {MatDialog} from "@angular/material/dialog";
 import {StoreService} from "../../../../helpers/services/store.service";
 import {HomeControlService} from "../../home-control.service";
 
